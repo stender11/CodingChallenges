77f7a247b5b525a4b6396ad4dbbf82f5d64a6c52
Adam Stephens <astephens1134@gmail.com>
2022-05-05 22:34:50 -0400

    1st codewars challenge upload

---
+++ CC-2022-03-24/js/main.js
@@ -0,0 +1,14 @@
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+function greet() {
+    return "hello world"
+}
+
+console.log(greet())
+
+// Alternate solution using an expression
+
+// const greet = () => {
+//     alert("hello world!");
+// }
+// greet()
