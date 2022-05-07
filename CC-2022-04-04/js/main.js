2f64ecb62ad4ded49856de41e3d50cfa57aa7ba0
Adam Stephens <astephens1134@gmail.com>
2022-05-07 12:49:46 -0400

    add refactored codewars challenge solution from 2022-04-04

---
+++ CC-2022-04-04/js/main.js
@@ -0,0 +1,7 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+const evenOrOdd = number => number % 2 === 0 ? "Even" : "Odd"
+
+console.log(evenOrOdd(7))
+console.log(evenOrOdd(77))
+console.log(evenOrOdd(88))
