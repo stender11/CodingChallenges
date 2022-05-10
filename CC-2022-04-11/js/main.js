5aa741732722857e6496326c98bc7fc2095d4bd5
Adam Stephens <astephens1134@gmail.com>
2022-05-10 14:09:54 -0400

    add refactored code challenge solution from 2022-04-11

---
+++ CC-2022-04-11/js/main.js
@@ -0,0 +1,18 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+function makeNegative(num) {
+    if (num < 0) {
+        return num
+    } else if (num > 0) {
+        return (num * -1)
+    } else {
+        return 0
+    }
+}
+
+console.log(makeNegative(1))    // return -1
+console.log(makeNegative(-5))   // return -5
+console.log(makeNegative(0))    // return 0
+console.log(makeNegative(0.12)) // return -0.12
