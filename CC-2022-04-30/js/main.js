2a66e0558ca7c9791cfb497015acae946f3e1658
Adam Stephens <astephens1134@gmail.com>
2022-05-22 19:56:47 -0400

    add refactored code solution for boolean to string

---
+++ CC-2022-04-30/js/main.js
@@ -0,0 +1,11 @@
+// ---Instructions:---
+// Implement a function which convert the given boolean value into its string representation.
+// Note: Only valid inputs will be given.
+
+// ---Submitted Solution:---
+function booleanToString(b){
+    return b.toString()
+}
+
+// ---Test cases:---
+console.log(booleanToString(true))
