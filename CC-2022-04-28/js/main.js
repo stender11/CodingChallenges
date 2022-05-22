4684656e6a30f69e377b34919e0985a9961c65c5
Adam Stephens <astephens1134@gmail.com>
2022-05-22 19:52:53 -0400

    add refactored code solution using map method

---
+++ CC-2022-04-28/js/main.js
@@ -0,0 +1,8 @@
+// ---Instructions:---
+// Given an array of integers, return a new array with each value doubled.
+
+// ---Submitted Solution:---
+const maps = x => x.map(e => e * 2)
+
+// ---Test cases:---
+console.log(maps([1, 2, 3])) // --> [2, 4, 6]
