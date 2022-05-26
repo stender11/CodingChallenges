1d41950c147416a828d3847fe9ee7d8a40743ad2
Adam Stephens <astephens1134@gmail.com>
2022-05-26 11:04:19 -0400

    add refactored code solution for string to uppercase

---
+++ CC-2022-05-05/js/main.js
@@ -0,0 +1,13 @@
+// ---Instructions:---
+// Write a function which converts the input string to uppercase.
+
+// ---Submitted Solution:---
+function makeUpperCase(str) {
+    return str.toUpperCase()
+}
+
+// alternate solution
+// const makeUpperCase = str => str.toUpperCase()
+
+// ---Test cases:---
+console.log(makeUpperCase("hola"))
