211f60db58842552974b46edd36abe44fdd4698c
Adam Stephens <astephens1134@gmail.com>
2022-06-02 15:56:39 -0400

    add 6kyu code challenge solution for replacing characters and duplicate chars in a string

---
+++ 6-kyu/duplicateEncode.js
@@ -0,0 +1,24 @@
+// ---Instructions:---
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+// Notes
+// Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
+// ---Submitted Solution:---
+function duplicateEncode(word) {
+    word = word.toLowerCase()
+    str = ""
+    for (i = 0; i < word.length; i++) {
+        if (word.indexOf(word[i]) == word.lastIndexOf(word[i])) {
+            str += "("
+        } else if (word.indexOf(word[i]) !== word.lastIndexOf(word[i])) {
+            str += ")"
+        }
+    }
+    return str
+}
+
+// ---Test cases:---
+console.log(duplicateEncode("din")) // =>  "((("
+console.log(duplicateEncode("recede")) // =>  "()()()"
+console.log(duplicateEncode("Success")) // =>  ")())())"
+console.log(duplicateEncode("(( @")) // =>  "))((" 
