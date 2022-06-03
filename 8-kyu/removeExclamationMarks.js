a96efa45edbcc4ba5c41fcef92e409d837f7e775
Adam Stephens <astephens1134@gmail.com>
2022-06-03 13:34:42 -0400

    add three 8kyu code challenge solutions

---
+++ 8-kyu/removeExclamationMarks.js
@@ -0,0 +1,5 @@
+// ---Instructions:---
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+// ---Submitted Solution:---
+const removeExclamationMarks = str => str.split("!").join("")
