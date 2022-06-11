a27d815fdcf341cf5809ffbea60eaa9247451328
Adam Stephens <astephens1134@gmail.com>
2022-06-11 19:14:49 -0400

    add three 8kyu code challenge solutions

---
+++ 8-kyu/twiceAsOld.js
@@ -0,0 +1,14 @@
+// ---Instructions:---
+// Your function takes two arguments:
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+// ---Submitted Solution:---
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    let howMany = sonYearsOld * 2 - dadYearsOld
+    return Math.abs(howMany)
+}
+
+// ---Test cases:---
+console.log(twiceAsOld(36,7)) // 22
