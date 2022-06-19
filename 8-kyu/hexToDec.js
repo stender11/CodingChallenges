4eef848822d9ea4663b6532fc226e347d7e83796
Adam Stephens <astephens1134@gmail.com>
2022-06-19 09:01:06 -0400

    add one 8kyu code challenge solution

---
+++ 8-kyu/hexToDec.js
@@ -0,0 +1,10 @@
+// ---Instructions:---
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+// ---Submitted Solution:---
+function hexToDec(hexString){
+    return Number.parseInt(hexString, 16)
+}
+
+// ---Test cases:---
+console.log(hexToDec("a")) // 10
