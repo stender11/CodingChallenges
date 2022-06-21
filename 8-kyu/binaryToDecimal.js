ade8f584d8496a0ff5c70b6661a69ac5a8219327
Adam Stephens <astephens1134@gmail.com>
2022-06-21 10:25:05 -0400

    add one 8kyu code challenge solution

---
+++ 8-kyu/binaryToDecimal.js
@@ -0,0 +1,10 @@
+// ---Instructions:---
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+// ---Submitted Solution:---
+function binToDec(bin){
+    return parseInt(bin, 2)
+}
+
+// ---Test cases:---
+console.log(binToDec(1001001))
