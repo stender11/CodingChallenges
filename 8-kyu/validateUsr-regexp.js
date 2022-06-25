90d1df127d17588061958560ea34b976d3e7ca9e
Adam Stephens <astephens1134@gmail.com>
2022-06-25 13:06:27 -0400

    add one 8kyu code challenge solution

---
+++ 8-kyu/validateUsr-regexp.js
@@ -0,0 +1,24 @@
+// ---Instructions:---
+// Write a simple regex to validate a username. Allowed characters are:
+// lowercase letters,
+// numbers,
+// underscore
+// Length should be between 4 and 16 characters (both included).
+
+// ---Submitted Solution:---
+function validateUsr(username) {
+    res = /^[a-z0-9_]{4,16}$/.test(username)
+    return res
+}
+
+// ---Test cases:---
+console.log(validateUsr('asddsa')) // true
+console.log(validateUsr('a')) // false
+console.log(validateUsr('Hass')) // false
+console.log(validateUsr('Hasd_12assssssasasasasasaasasasasas')) // false
+console.log(validateUsr('')) // false
+console.log(validateUsr('____')) // true
+console.log(validateUsr('012')) // false
+console.log(validateUsr('p1pp1')) // true
+console.log(validateUsr('asd43 34')) // false
+console.log(validateUsr('asd43_34')) // true
