2aff270e97f8acf905aeb1e631e84911ea6d0cfe
Adam Stephens <astephens1134@gmail.com>
2022-06-27 00:28:07 -0400

    add one 8kyu code challenge solution

---
+++ 8-kyu/unusualFive.js
@@ -0,0 +1,12 @@
+// ---Instructions:---
+// Write a function that always returns 5
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+// Good luck :)
+
+// ---Submitted Solution:---
+function unusualFive() {
+    return ["not one", "not two", "not three", "not four", "not five"].length
+}
+
+// ---Test cases:---
+console.log(unusualFive()) // === 5
