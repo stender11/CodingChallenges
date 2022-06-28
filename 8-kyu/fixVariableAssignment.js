4848f1bf26663db7d79b66a7a3cc1fbbee6a407d
Adam Stephens <astephens1134@gmail.com>
2022-06-28 16:09:48 -0400

    add one 8kyu code challenge solution

---
+++ 8-kyu/fixVariableAssignment.js
@@ -0,0 +1,12 @@
+// ---Instructions:---
+// Variable assignment
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+// ---Submitted Solution:---
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
+
+// ---Test cases:---
+console.log(name)
