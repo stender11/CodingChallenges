21e2e64eaac0bf70734e6c42df6bb6c67ca9ca60
Adam Stephens <astephens1134@gmail.com>
2022-07-03 19:40:30 -0400

    add one 8kyu code challenge solution

---
+++ 8-kyu/printArrayjoin.js
@@ -0,0 +1,13 @@
+// ---Instructions:---
+// Input: Array of elements
+// ["h","o","l","a"]
+// Output: String with comma delimited elements of the array in th same order.
+// "h,o,l,a"
+
+// ---Submitted Solution:---
+function printArray(array){
+    return array.join()
+}
+
+// ---Test cases:---
+console.log(printArray(["h","o","l","a"])) // --> "h,o,l,a"
