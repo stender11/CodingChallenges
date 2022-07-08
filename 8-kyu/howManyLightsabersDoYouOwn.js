6ab6957c5fcf2baa22f1d7003c5395f94f675466
Adam Stephens <astephens1134@gmail.com>
2022-07-08 15:04:14 -0400

    add one 8kyu code challenge solution

---
+++ 8-kyu/howManyLightsabersDoYouOwn.js
@@ -0,0 +1,13 @@
+// ---Instructions:---
+// Inspired by the development team at Vooza, write the function that
+// accepts the name of a programmer, and
+// returns the number of lightsabers owned by that person.
+// The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+// Note: your function should have a default parameter.
+
+// ---Submitted Solution:---
+const howManyLightsabersDoYouOwn = name => name === "Zach" ? 18 : null
+
+// ---Test cases:---
+console.log(howManyLightsabersDoYouOwn("anyone else")) // --> 0
+console.log(howManyLightsabersDoYouOwn("Zach")) // --> 18
