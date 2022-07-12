c7e9719ca23fa8dcccd8ca192b59118e943fbdb4
Adam Stephens <astephens1134@gmail.com>
2022-07-12 02:09:22 -0400

    add one 8kyu code challenge solution

---
+++ 8-kyu/getCharCode.js
@@ -0,0 +1,10 @@
+// ---Instructions:---
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+// ---Submitted Solution:---
+function getChar(c) {
+  return String.fromCharCode(c)
+}
+
+// ---Test cases:---
+console.log(getChar(65)) // 'A'
