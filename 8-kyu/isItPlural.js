8349049d3c6c404acbeef766e7f725bcc0464495
Adam Stephens <astephens1134@gmail.com>
2022-07-12 02:09:57 -0400

    add one 8kyu code challenge solution

---
+++ 8-kyu/isItPlural.js
@@ -0,0 +1,15 @@
+// ---Instructions:---
+// We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not. This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
+// You only need to worry about english grammar rules for this kata, where anything that isn't singular (one of something), it is plural (not one of something).
+// All values will be positive integers or floats, or zero.
+
+// ---Submitted Solution:---
+function plural(n) {
+  return n !== 1
+}
+
+// ---Test cases:---
+console.log(plural(0)) // true
+console.log(plural(0.5)) // true
+console.log(plural(1)) // false
+console.log(plural(Infinity)) // true
