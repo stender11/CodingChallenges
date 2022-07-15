1eb4cfdfb01d8f6c2f04967970f763900dcba738
Adam Stephens <astephens1134@gmail.com>
2022-07-15 04:00:06 -0400

    add one 8kyu code challenge solution

---
+++ 8-kyu/websitesArrayUnshift.js
@@ -0,0 +1,11 @@
+// ---Instructions:---
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+// The websites array has already been defined for you using the following code:
+
+// ---Submitted Solution:---
+var websites = []
+websites.unshift("codewars")
+
+// ---Test cases:---
+console.log(websites)
