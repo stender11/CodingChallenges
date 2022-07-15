651a4ede1d090bcb49cda29d0db6b781d4ddeefc
Adam Stephens <astephens1134@gmail.com>
2022-07-15 04:13:25 -0400

    add one 8kyu code challenge solution

---
+++ 8-kyu/animalObject.js
@@ -0,0 +1,23 @@
+// ---Instructions:---
+// In javascript, Object is one of basic data types. Define an Object can use var obj=new Object() or var obj={}
+// You can define the object attributes during initialization, like this:
+// var animal={name:"dog"}
+// you can also set/get some properties after the object definition, like this:
+// var animal={}
+// animal.name="dog"  (or animal["name"]="dog")
+// Task
+// Give you a function animal, accept 1 parameter:obj like this:
+// {name:"dog",legs:4,color:"white"}
+// and return a string like this:
+// "This white dog has 4 legs."
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+// ---Submitted Solution:---
+function animal(obj){
+    return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
+}
+  
+// ---Test cases:---
+console.log(animal({name:"dog",legs:4,color:"white"})) // "This white dog has 4 legs."
+console.log(animal({name:"cock",legs:2,color:"red"})) // "This red cock has 2 legs."
+console.log(animal({name:"rabbit",legs:4,color:"gray"})) // "This gray rabbit has 4 legs."
