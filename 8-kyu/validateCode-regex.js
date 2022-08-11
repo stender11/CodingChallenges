c3ae8401b6f0ea649e5456f122a1d9bd0747b5bb
Adam Stephens <astephens1134@gmail.com>
2022-08-10 20:02:28 -0400

    add one 8kyu code challenge solution

---
+++ 8-kyu/validateCode-regex.js
@@ -0,0 +1,13 @@
+// ---Instructions:---
+// Basic regex tasks. Write a function that takes in a numeric code of any length. The function should check if the code begins with 1, 2, or 3 and return true if so. Return false otherwise.
+// You can assume the input will always be a number.
+
+// ---Submitted Solution:---
+function validateCode(code) {
+    return /^[1-3]/.test(code)
+}
+
+// ---Test cases:---
+console.log(validateCode(123)) // true
+console.log(validateCode(248)) // true
+console.log(validateCode(8)) // false
