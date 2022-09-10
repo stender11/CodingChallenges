df0088407942b68ca9defe891e66a2921342e8b8
Adam Stephens <astephens1134@gmail.com>
2022-09-10 12:05:34 -0400

    add one 7kyu code challenge solution

---
+++ 7-kyu/binaryArrayToNumber.js
@@ -0,0 +1,30 @@
+// ---Instructions:---
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+// Examples:
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
+// However, the arrays can have varying lengths, not just limited to 4.
+
+// ---Submitted Solution:---
+// const binaryArrayToNumber = arr => {
+//     return parseInt(arr.join(""), 2)
+// }
+
+// alternate solution
+const binaryArrayToNumber = arr => {
+    return arr.reduce((acc, cv) => (acc = acc * 2 + cv), 0)
+}
+
+// ---Test cases:---
+console.log(binaryArrayToNumber([0,0,0,1])) // 1
+console.log(binaryArrayToNumber([0,0,1,0])) // 2
+console.log(binaryArrayToNumber([1,1,1,1])) // 15
+console.log(binaryArrayToNumber([0,1,1,0])) // 6
+console.log(binaryArrayToNumber([1,0,1,1,0])) // 22
