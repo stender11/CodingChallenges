ecbf04c7dc8ce034cdd74ef5f8eb1b869d7a651f
Adam Stephens <astephens1134@gmail.com>
2022-09-18 14:54:23 -0400

    add one 7kyu code challenge solution

---
+++ 7-kyu/smallEnough.js
@@ -0,0 +1,14 @@
+// ---Instructions:---
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+// You can assume all values in the array are numbers.
+
+// ---Submitted Solution:---
+function smallEnough(a, limit) {
+    return a.every(e => e <= limit)
+}
+
+// ---Test cases:---
+console.log(smallEnough([66, 101], 200)) // true
+console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100)) // false
+console.log(smallEnough([101, 45, 75, 105, 99, 107], 107)) // true
+console.log(smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120)) // true 
