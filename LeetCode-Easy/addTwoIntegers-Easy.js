52b6665c010a16b7a1adf89b16680067c5ad58ec
Adam Stephens <astephens1134@gmail.com>
2022-10-23 20:01:33 -0400

    add one easy leetcode challenge solution

---
+++ LeetCode-Easy/addTwoIntegers-Easy.js
@@ -0,0 +1,22 @@
+// ---Instructions:---
+// 2235. Add Two Integers
+// Given two integers num1 and num2, return the sum of the two integers.
+// Constraints:
+// -100 <= num1, num2 <= 100
+
+// ---P.R.E.P.---
+// Example 1:
+// Input: num1 = 12, num2 = 5
+// Output: 17
+// Explanation: num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is returned.
+// Example 2:
+// Input: num1 = -10, num2 = 4
+// Output: -6
+// Explanation: num1 + num2 = -6, so -6 is returned.
+
+// ---Submitted Solution:---
+const sum = (num1, num2) => num1 + num2
+
+// ---Test cases:---
+console.log(sum(num1 = 12, num2 = 5)) // Answer: 17
+console.log(sum(num1 = -10, num2 = 4)) // Answer: -6
