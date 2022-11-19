4d84f35709f1eff806dadfe5a73258fcfbee374e
Adam Stephens <astephens1134@gmail.com>
2022-11-19 17:56:03 -0500

    add one 6kyu code challenge solution

---
+++ 6-kyu/spinWords-6kyu.js
@@ -0,0 +1,22 @@
+// ---Instructions:---
+// Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+// ---P.R.E.P.---
+// Examples:
+// spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" 
+// spinWords( "This is a test") => returns "This is a test" 
+// spinWords( "This is another test" )=> returns "This is rehtona test"
+
+// ---Submitted Solution:---
+function spinWords(string) {
+    return string.split(" ").map(word => word.length >= 5 ? word.split("").reverse().join("") : word).join(" ")
+}
+
+// ---Test cases:---
+console.log(spinWords("Welcome")); // "emocleW"
+console.log(spinWords("Hey fellow warriors")); // "Hey wollef sroirraw"
+console.log(spinWords("This is a test")); // "This is a test"
+console.log(spinWords("This is another test")); // "This is rehtona test"
+console.log(spinWords("You are almost to the last test")); // "You are tsomla to the last test"
+console.log(spinWords("Just kidding there is still one more")); // "Just gniddik ereht is llits one more"
+console.log(spinWords("Seriously this is the last one")); // "ylsuoireS this is the last one"
