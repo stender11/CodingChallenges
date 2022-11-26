3ca747baf4f1d0db9e5422a3354386ebcd87a498
Adam Stephens <astephens1134@gmail.com>
2022-11-25 23:37:02 -0500

    add one 7kyu code challenge solution

---
+++ 7-kyu/getSum-7kyu.js
@@ -0,0 +1,25 @@
+// ---Instructions:---
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+// Note: a and b are not ordered!
+
+// ---P.R.E.P.---
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+// ---Submitted Solution:---
+function getSum(a, b) {
+    const min = Math.min(a, b);
+    const max = Math.max(a, b);
+    return (max - min + 1) * (min + max) / 2;
+}
+
+// ---Test cases:---
+console.log(getSum(0, -1)); // -1
+console.log(getSum(0, 1)); // 1
+console.log(getSum(1, 1)); // 1
+console.log(getSum(-1, 2)); // 2
