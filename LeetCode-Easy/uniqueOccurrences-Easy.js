ed17d4ccbc93e884f869aa32a06cb4621d29dfa3
Adam Stephens <astephens1134@gmail.com>
2023-01-06 19:02:16 -0500

    added one easy leetcode challenge solution

---
+++ LeetCode-Easy/uniqueOccurrences-Easy.js
@@ -0,0 +1,34 @@
+// ---Instructions:---
+// LeetCode 1207 - Unique Number of Occurrences
+// Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+// Constraints:
+// 1 <= arr.length <= 1000
+// -1000 <= arr[i] <= 1000
+
+// ---P.R.E.P.---
+// Example 1:
+// Input: arr = [1,2,2,1,1,3]
+// Output: true
+// Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+// Example 2:
+// Input: arr = [1,2]
+// Output: false
+// Example 3:
+// Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+// Output: true
+
+// ---Submitted Solution:---
+function uniqueOccurrences(arr) {
+    let numMap = {};
+
+    for (num of arr) {
+        numMap[num] = numMap[num] + 1 || 1;
+    }
+
+    return new Set(Object.values(numMap)).size === Object.keys(numMap).length;
+};
+
+// ---Test cases:---
+console.log(uniqueOccurrences([1,2,2,1,1,3])); // true
+console.log(uniqueOccurrences([1,2])); // false
+console.log(uniqueOccurrences([-3,0,1,-3,1,1,1,-3,10,0])); // true
