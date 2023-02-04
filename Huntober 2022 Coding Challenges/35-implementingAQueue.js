258dc9b7a5a4a067e91e0511ea4c91e8ea992ce5
Adam Stephens <astephens1134@gmail.com>
2023-02-04 18:28:06 -0500

    added one 7kyu codewars challenge solution

---
+++ Huntober 2022 Coding Challenges/35-implementingAQueue.js
@@ -0,0 +1,47 @@
+// ---Instructions:---
+// Codewars 7kyu - Implementing a Queue
+// Queues are linear collections of objects that can be inserted and removed in a FIFO (first in, first out) manner.
+// An example of a queue in real life and not strictly computing would simply be the dreaded waiting line, i.e.the first person in line is also the first one who gets out. 
+// Waiting Lines
+// In this problem, we are going to be implementing our own enqueue, dequeue, and size methods for the queue constructor we are creating, so we should be able to create new instances of the Queue.
+// The enqueue method takes in the item as a parameter, while the dequeue method does not.
+// The size method simply returns the number of items in the queue.
+// To enqueue an item into the queue means to insert an item into the back, or tail, of the queue.
+// To dequeue an item means means to remove the item at the front, or head, of the queue.
+// In a queue, we remove the item the least recently added.
+// JavaScript Methodology
+// Queues can be implemented in JavaScript using arrays.
+// You can use the built in push or unshift functions in order to add items to the queue array as well as the shift or pop to remove them.
+// As long as the tests pass, go for it!
+
+// ---P.R.E.P.---
+// Hint:  Create a class that has four methods: constructor - creates array, enqueue - unshift, dequeue - pop, and size - length
+
+// ---Submitted Solution:---
+class Queue {
+    constructor() {
+        this.list = []
+    }
+ 
+    size() {
+        return this.list.length
+    };
+  
+    enqueue(item) {
+        this.list.push(item)
+    };
+  
+    dequeue() {
+        return this.list.shift()
+    };
+};
+  
+// ---Test cases:---
+let stack = new Queue()
+console.log(stack.size()); // 0
+console.log(stack.enqueue("cat")); // 
+console.log(stack.enqueue("cat")); // 
+console.log(stack.enqueue("cat")); // 
+console.log(stack.size()); // 0
+console.log(stack.dequeue()); // 0
+console.log(stack.list); // 0
