f83b1b7549cf68ad6d2f504c7936a24ace6b2058
Adam Stephens <astephens1134@gmail.com>
2023-02-11 00:12:06 -0500

    added one 8kyu codewars challenge solution

---
+++ 7-kyu/divisors-7kyu.js
@@ -0,0 +1,24 @@
+// ---Instructions:---
+// Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+
+// ---P.R.E.P.---
+// Example:
+// divisors(12); // should return [2,3,4,6]
+// divisors(25); // should return [5]
+// divisors(13); // should return "13 is prime"
+
+// ---Submitted Solution:---
+function divisors(integer) {
+    let divisorArr = [];
+
+    for (let i = 2; i < integer; i++) {
+        if (integer % i === 0) divisorArr.push(i);
+    }
+
+    return divisorArr.length > 0 ? divisorArr : `${integer} is prime`;
+};
+
+// ---Test cases:---
+console.log(divisors(15)); // [3, 5]
+console.log(divisors(12)); // [2, 3, 4, 6]
+console.log(divisors(13)); // "13 is prime"
