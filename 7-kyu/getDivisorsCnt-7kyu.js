d61ec9f1320f5d7b6a3d39139609a6c4cdba5863
Adam Stephens <astephens1134@gmail.com>
2023-02-16 23:41:08 -0500

    added one 7kyu codewars challenge solution

---
+++ 7-kyu/getDivisorsCnt-7kyu.js
@@ -0,0 +1,28 @@
+// ---Instructions:---
+// Count the number of divisors of a positive integer n.
+// Random tests go up to n = 500000.
+// Note you should only return a number, the count of divisors. The numbers between parentheses are shown only for you to see which numbers are counted in each case.
+
+// ---P.R.E.P.---
+// Examples (input --> output)
+// 4 --> 3 (1, 2, 4)
+// 5 --> 2 (1, 5)
+// 12 --> 6 (1, 2, 3, 4, 6, 12)
+// 30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+
+// ---Submitted Solution:---
+function getDivisorsCnt(num) {
+    let divisorArr = [];
+
+    for (let i = 1; i <= num; i++) {
+        if (num % i === 0) divisorArr.push(i);
+    }
+
+    return divisorArr.length;
+};
+
+// ---Test cases:---
+console.log(getDivisorsCnt(1)); // 1
+console.log(getDivisorsCnt(10)); // 4
+console.log(getDivisorsCnt(11)); // 2
+console.log(getDivisorsCnt(54)); // 8
