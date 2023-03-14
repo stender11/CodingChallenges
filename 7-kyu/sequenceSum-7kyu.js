06dded5d9e8bae70dfeae8db9e191fa21c813dd5
Adam Stephens <astephens1134@gmail.com>
2023-03-13 22:30:37 -0400

    added one 7kyu codewars solution

---
+++ 7-kyu/sequenceSum-7kyu.js
@@ -0,0 +1,29 @@
+// ---Instructions:---
+// Your task is to write a function which returns the sum of a sequence of integers.
+// The sequence is defined by 3 non-negative values: begin, end, step.
+// If begin value is greater than the end, your function should return 0. If end is not the result of an integer number of steps, then don't add it to the sum. See the 4th example below.
+
+// ---P.R.E.P.---
+// Examples
+// 2,2,2 --> 2
+// 2,6,2 --> 12 (2 + 4 + 6)
+// 1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+// 1,5,3  --> 5 (1 + 4)
+
+// ---Submitted Solution:---
+const sequenceSum = (begin, end, step) => {
+    let sum = 0;
+
+    if (begin > end) return 0;
+
+    for (let i = begin; i <= end; i += step) {
+        sum += i;
+    }
+
+    return sum;
+};
+
+// ---Test cases:---
+console.log(sequenceSum(2, 6, 2), 12)
+console.log(sequenceSum(1, 5, 1), 15)
+console.log(sequenceSum(1, 5, 3), 5)
