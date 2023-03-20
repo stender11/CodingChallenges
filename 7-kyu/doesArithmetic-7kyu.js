f7f1300749eab543ba8a095b525a7a9cafb86ce5
Adam Stephens <astephens1134@gmail.com>
2023-03-19 22:25:59 -0400

    added one 7kyu codewars solution

---
+++ 7-kyu/doesArithmetic-7kyu.js
@@ -0,0 +1,32 @@
+// ---Instructions:---
+// Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+// a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
+// The four operators are "add", "subtract", "divide", "multiply".
+// Try to do it without using if statements!
+
+// ---P.R.E.P.---
+// A few examples:(Input1, Input2, Input3 --> Output)
+// 5, 2, "add"      --> 7
+// 5, 2, "subtract" --> 3
+// 5, 2, "multiply" --> 10
+// 5, 2, "divide"   --> 2.5
+
+// ---Submitted Solution:---
+const arithmetic = (a, b, oper) => {
+    switch (oper) {
+        case "add":
+            return a + b;
+        case "subtract":
+            return a - b;
+        case "multiply":
+            return a * b;
+        case "divide":
+            return a / b;
+    }
+};
+
+// ---Test cases:---
+console.log(arithmetic(1, 2, "add"), 3);
+console.log(arithmetic(8, 2, "subtract"), 6);
+console.log(arithmetic(5, 2, "multiply"), 10);
+console.log(arithmetic(8, 2, "divide"), 4);
