6e899445c0b146065a28819b324010a571ba96ea
Adam Stephens <astephens1134@gmail.com>
2023-03-21 22:44:25 -0400

    added one 7kyu codewars solution

---
+++ 7-kyu/inAscOrder-7kyu.js
@@ -0,0 +1,26 @@
+// ---Instructions:---
+// Are the numbers in order?
+// In this Kata, your function receives an array of integers as input. Your task is to determine whether the numbers are in ascending order. An array is said to be in ascending order if there are no two adjacent integers where the left integer exceeds the right integer in value.
+// For the purposes of this Kata, you may assume that all inputs are valid, i.e. arrays containing only integers.
+// Note that an array of 0 or 1 integer(s) is automatically considered to be sorted in ascending order since all (zero) adjacent pairs of integers satisfy the condition that the left integer does not exceed the right integer in value.
+// N.B. If your solution passes all fixed tests but fails at the random tests, make sure you aren't mutating the input array.
+
+// ---P.R.E.P.---
+// For example:
+// inAscOrder([1,2,4,7,19]); // returns true
+// inAscOrder([1,2,3,4,5]); // returns true
+// inAscOrder([1,6,10,18,2,4,20]); // returns false
+// inAscOrder([9,8,7,6,5,4,3,2,1]); // returns false because the numbers are in DESCENDING order
+
+// ---Submitted Solution:---
+const inAscOrder = arr => {
+    return arr.every((num, i) => {
+        return i === 0 || num >= arr[i - 1];
+    });
+};
+
+// ---Test cases:---
+console.log(inAscOrder([1, 2, 4, 7, 19]), true);
+console.log(inAscOrder([1, 2, 3, 4, 5]), true);
+console.log(inAscOrder([1, 6, 10, 18, 2, 4, 20]), false);
+console.log(inAscOrder([9, 8, 7, 6, 5, 4, 3, 2, 1]), false);
